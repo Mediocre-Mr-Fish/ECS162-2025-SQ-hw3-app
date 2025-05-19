0e0e78c7fc7eb24f74cb4150df4adf09e2a482bb
Ellison Song <ellsong@ucdavis.edu>
2025-05-18 23:33:50 -0700

    Added more comments to backend. Updated README.md

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Ellison Song: ellsong@ucdavis.edu
 
 Sean Singleton: ssingleton@ucdavis.edu
 
-SVG sources linked in HTML comments or in .svg file comments.
+SVG sources linked in HTML comments or in the .svg file itself when opened in a text editor.
 
 ## Git
 
-Repository at [`https://github.com/Mediocre-Mr-Fish/ECS162-2025-SQ-hw3-app`](<https://github.com/Mediocre-Mr-Fish/ECS162-2025-SQ-hw3-app>)
+Repository at [`https://github.com/Mediocre-Mr-Fish/ECS162-2025-SQ-hw3-app`](<https://github.com/Mediocre-Mr-Fish/ECS162-2025-SQ-hw3-app>).
 
 Commit history only covers from importing files from Homework 2.
 
@@ -26,19 +26,26 @@ To redact a portion of a comment, as a moderator, press the 'Redact' button, hig
 
 ### Security
 
-I have asked the TAs about login security, and they have explicitly stated that security is not an objective of this assignment. In their example, a login is handled by redirecting with the account's email as a URL parameter. A similar method is used here.
+I have asked the TAs about login security, and they have explicitly stated that security is not an objective of this assignment.
+In their example, a login is handled by redirecting with the account's email as a URL parameter.
+A similar method is used here.
 
 ### Admin and Permissions
 
-Admin accounts are not mentioned in the Homework directions. Thus, they are treated identically to moderators here.
+Admin accounts are not mentioned in the Homework directions.
+Thus, they are treated identically to moderators here.
 
-Account permissions are stored in the database, initialized by Flask (See [Users](#Users) below for more info) on line `182` in `app.py`:
+Account permissions are stored in the database, initialized by Flask (See [Users](#Users) below for more info) on line `238` in `app.py`:
 ```Python
 initalizePermissionsDB()
 ```
-using information on lines `156` to `157` in `app.py`:
+
+Permission information is stored on lines `197` to `200` in `app.py`:
+
 ```Python
+# List of users that are moderators
 USERS_MODERATOR = ["admin@hw3.com", "moderator@hw3.com"]
+# List of permissions
 PERMISSIONS = {"can_remove_comments": USERS_MODERATOR}
 ```
 
@@ -47,36 +54,53 @@ PERMISSIONS = {"can_remove_comments": USERS_MODERATOR}
 Despite following the instructions in the discussion, a database collection for users is *not* automatically generated upon building the container.
 Instead, Flask backend will generate this collection on startup.
 
-User information is stored on lines `136` to `155` in `app.py`, which is identical to the information found in `dex.yaml` and `dex_final.yaml`.
+The database that is created is named "commentsdb", with collections "comments", "users", and "permissions".
+Database and collection names are defined on lines `120` to `124` in `app.py`:
+```Python
+# names of databases and collections
+DB_COMMENTS = "commentsdb"
+COL_COMMENTS = "comments"
+COL_USERS = "users"
+COL_PERMISSIONS = "permissions"
+```
 
-User information collection is initialized on line `181` in `app.py`:
+User information is stored on lines `176` to `195` in `app.py`, which is identical to the information found in `dex.yaml` and `dex_final.yaml`.
+This means that the example user `alice@example.com` seen in discussion is not included.
+
+User information collection is initialized on line `236` in `app.py`:
 ```Python
 initalizeUsersDB()
 ```
 
 ### Sample Comments
 
-Flask backend includes code to add sample comments to a specific article.
-This can be removed or changed on line `132` of `app.py`:
+The Flask backend includes code to add sample comments to a specific article.
+This can be removed or changed on line `167` of `app.py`:
 ```Python
 addTestComments("d38b9aef-ab20-51c2-883c-94aa475b7273")
 ```
 
 By default, it only adds these comments to the article "[We Mapped Heat in 3 U.S. Cities. Some Sidewalks Were Over 130 Degrees.](<https://www.nytimes.com/interactive/2024/07/17/climate/heat-map-phoenix-sacramento-portland.html>)", which is the first article to appear when filtering by location.
 
-To add sample comments to a different article, the article ID can be changed. To find the ID of an article, it can be viewed in the consle. 
+To add sample comments to a different article, the article ID can be changed.
+To find the ID of an article, it can be viewed in the console.
+Only articles that have no comments can have these sample comments added.
 
 ![Screenshot of browser console. A response JSON object is shown. Dropdowns are expanded to show more information. Under [response/docs/0], [_id: "nyt://interactive/d38b9aef-ab20-51c2-883c-94aa475b7273"] is hovered over. [d38b9aef-ab20-51c2-883c-94aa475b7273] is highlighted.](readme_src/article_id.png)
 
 ### Article Queries
 
-Articles fetched are, by default, filtered to the Davis/Sacramento area using NYT's tagging system. All articles included have a `Location` tag of `Davis (Calif)` or `Sacramento (Calif)`. The tags for fetched articles can be found in the console output:
+Articles fetched are, by default, filtered to the Davis/Sacramento area using NYT's tagging system.
+All articles included have a `Location` tag of `Davis (Calif)` or `Sacramento (Calif)`.
+The tags for fetched articles can be found in the console output:
 
 ![Screenshot of browser console. A response JSON object is shown. Dropdowns are expanded to show more information. Under [response/docs/0/keywords/4], a JSON object associates [name:'Location'] with [value:'Sacramento (Calif)']](readme_src/location_tag.png)
 
 When searching for articles using the search bar, this default filter is overriden.
 
-This differs from our Homework 2 submission, which simply defaulted to searching for "Davis" when the search bar was empty. That approach did not fufill the localization requiremnt, and was worth no credit. According to a TA:
+This differs from our Homework 2 submission, which simply defaulted to searching for "Davis" when the search bar was empty.
+That approach did not fufill the localization requiremnt, and was worth no credit.
+According to a TA:
 
 > When I run your code, it appears that only articles that contain "davis" are returned. The assignment asked for stories from the Davis/Sacramento area. We are looking for articles localized in Davis/Sacramento, not articles that contain the word "Davis." 
 
@@ -91,4 +115,5 @@ try {
 }
 ```
 
-The provided code simply searches for articles that contain "Davis" or "Sacramento", which is nearly identical to the method used in Homework 2, which recieved no credit for that requirement. Thus, we are using a different method for filtering that hopefully better fills that requirement.
+The provided code simply searches for articles that contain "Davis" or "Sacramento", which is nearly identical to the method used in Homework 2, which recieved no credit for that requirement.
+Thus, we are using a different method for filtering that hopefully better fills that requirement.
--- backend/app.py
+++ backend/app.py
@@ -8,14 +8,25 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-with open("debug_out.txt", "w") as f:
+# region Debug Output
+
+# for ease of debugging, debug output will be written to debug_out.txt
+DEBUG_FILE = "debug_out.txt"
+
+# clear debug file in startup
+with open(DEBUG_FILE, "w") as f:
     f.write("DEBUG LOG\n")
 
 
+# function to write to debug file
 def debug_out(message: str):
-    with open("debug_out.txt", "a") as f:
+    with open(DEBUG_FILE, "a") as f:
         f.write(str(message) + "\n")
 
+
+# endregion Debug Output
+
+# code provided by starter file
 
 oauth = OAuth(app)
 
@@ -36,6 +47,7 @@ oauth.register(
 )
 
 
+# Wrapper object for interacting with database
 class MongoWrapper:
     def __init__(self, uri):
         self.client = MongoClient(uri)
@@ -48,20 +60,33 @@ class MongoWrapper:
         col = self.getDatabase(dbName)[colName]
         return col
 
+    # Insert a single document into the specified collection in the specified collection
+    # The dict that is passed in will have an ObjectID added to it
     def insertDocument(self, dbName, colName, jsonObj):
         return self.getCollection(dbName, colName).insert_one(jsonObj)
 
+    # Find a singe document in the specified collection in the specified collection
+    # Returns with a the first matching document
+    # A document is a match if all keys:value pairs in the query dict are present and match
     def findDocument(self, dbName, colName, jsonObj={}):
         return self.getCollection(dbName, colName).find_one(jsonObj)
 
+    # Find all documents in the specified collection in the specified collection
+    # Returns with an iterable containing matching documents
+    # A document is a match if all keys:value pairs in the query dict are present and match
     def searchDocument(self, dbName, colName, jsonObj={}):
         return self.getCollection(dbName, colName).find(jsonObj)
 
+    # Update a singe document in the specified collection in the specified collection
+    # Updates with a the first matching document
+    # A document is a match if all keys:value pairs in the query dict are present and match
+    # All key:value pairs in the values dict will be added or override those in the document
     def updateDocument(self, dbName, colName, valuesToSet, jsonObj={}):
         update_operation = {"$set": valuesToSet}
         return self.getCollection(dbName, colName).update_one(jsonObj, update_operation)
 
 
+# A class to construct the data of a comment
 class Comment:
     def __init__(
         self,
@@ -79,6 +104,7 @@ class Comment:
         self.content: str = content
         self.removed: str = removed
 
+    # convert comment object to a dict/json for the database to read
     def toJson(self):
         return {
             "articleID": self.articleID,
@@ -91,20 +117,27 @@ class Comment:
         }
 
 
+# names of databases and collections
 DB_COMMENTS = "commentsdb"
 COL_COMMENTS = "comments"
 COL_USERS = "users"
 COL_PERMISSIONS = "permissions"
 
+# create a wrapper object using the URI
 mongo = MongoWrapper(os.getenv("MONGO_URI"))
 
 
+# Function to insert a sample comment and a sample reply to an article
+# ONLY IF it does not have any comments yet.
 def addTestComments(articleID):
+    # check if any documents (comments) exist
     exists = False
     for c in mongo.searchDocument(DB_COMMENTS, COL_COMMENTS, {"articleID": articleID}):
         exists = True
         break
+    # if not, create them
     if not exists:
+        # create and push a sample comment
         test_comment_id = mongo.insertDocument(
             DB_COMMENTS,
             COL_COMMENTS,
@@ -116,6 +149,7 @@ def addTestComments(articleID):
             ).toJson(),
         )
         # debug_out(test_comment_id.inserted_id)
+        # create and push a sample reply
         test_reply_id = mongo.insertDocument(
             DB_COMMENTS,
             COL_COMMENTS,
@@ -129,10 +163,16 @@ def addTestComments(articleID):
         )
 
 
+# Add sample comments to articles. More information can be found in README.md
 addTestComments("d38b9aef-ab20-51c2-883c-94aa475b7273")
+
+# print all comments
 for c in mongo.searchDocument(DB_COMMENTS, COL_COMMENTS):
     debug_out(dict(c))
 
+# User information for database.
+# Identical to the information found in `dex.yaml` and `dex_final.yaml`.
+# More information can be found in README.md
 USERS_REGISTERED = [
     {
         "email": "admin@hw3.com",
@@ -153,85 +193,137 @@ USERS_REGISTERED = [
         "userID": "789",
     },
 ]
+
+# List of users that are moderators
 USERS_MODERATOR = ["admin@hw3.com", "moderator@hw3.com"]
+# List of permissions
 PERMISSIONS = {"can_remove_comments": USERS_MODERATOR}
 
+# Message that replaces a removed comment
+COMMENT_REMOVAL_MESSAGE = "COMMENT REMOVED BY MODERATOR!"
+# Character that replaces redacted sections
+COMMENT_REDACTION_CHAR = "█"
 
+
+# Function to initialize the Users collection
 def initalizeUsersDB():
+    # check if any documents exist
     exists = False
     for c in mongo.searchDocument(DB_COMMENTS, COL_USERS):
         exists = True
         break
+    # if not, create them
     if not exists:
         debug_out("Creating Users DB")
         for u in USERS_REGISTERED:
             mongo.insertDocument(DB_COMMENTS, COL_USERS, u)
 
 
+# Function to initialize the permissions collection
 def initalizePermissionsDB():
+    # check if any documents exist
     exists = False
     for c in mongo.searchDocument(DB_COMMENTS, COL_PERMISSIONS):
         exists = True
         break
+    # if not, create them
     if not exists:
         debug_out("Creating Permissions DB")
         mongo.insertDocument(DB_COMMENTS, COL_PERMISSIONS, PERMISSIONS)
 
 
+# initialize the Users collection
 initalizeUsersDB()
+# initialize the Permissions collection
 initalizePermissionsDB()
 
+
+# region NYT API Calls
+
+
+# Route to return the API key
+# Depricated: frontend does not need the key
+# All API fetches should be done though the below routes
 # @app.route('/api/key')
 # def get_key():
 #     return jsonify({'apiKey': os.getenv('NYT_API_KEY')})
 
 
+# Route to search for articles via query
 @app.route("/api/articles-query/<int:page>/<string:query>")
 def fetch_article_query(page: int, query: str):
+    # get key
     key = os.getenv("NYT_API_KEY")
+    # call api
     return redirect(
         f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}&api-key={key}&page={page}",
         code=302,
     )
 
 
+# Route to search for articles via filter
 @app.route("/api/articles-filter/<int:page>/<string:filter>")
 def fetch_article_filter(page: int, filter: str):
+    # get key
     key = os.getenv("NYT_API_KEY")
+    # format filter
     filter = filter.replace(":", "%3A")
+    # call api
     return redirect(
         f"https://api.nytimes.com/svc/search/v2/articlesearch.json?fq={filter}&api-key={key}&page={page}",
         code=302,
     )
 
 
+# endregion NYT API Calls
+
+
+# region Comments Database CRUD operations
+
+
+# route to get all comments for an article
 @app.route("/api/comments/<string:articleid>")
 def getComments(articleid: str):
     try:
         comments = {}
         queued = {}
+
+        # search for all documents (comments) that match article ID
         for c in list(
             mongo.searchDocument(DB_COMMENTS, COL_COMMENTS, {"articleID": articleid})
         ):
+            # Convert to dictionary
             d = dict(c)
+            # convert ObjectID to string
             _id = str(d["_id"])
             d["_id"] = _id
+            # if this is a reply, add it to the replies queue
             if d["parentID"]:
                 queued[_id] = d
             else:
                 comments[_id] = d
 
+        # for all queued replies
         for _id, d in queued.items():
+            # add it to the replies list for its parent comment
             pid = d["parentID"]
             comments[pid]["replies"].append(d)
 
+        # convert dictionary to list
         comments_list = list(comments.values())
+        # show in reverse chronological order
         comments_list.reverse()
         return jsonify({"comments": comments_list})
     except Exception as e:
         return jsonify({"Internal error": str(e)})
 
 
+# Route to post a comment via POST method
+# Requires the poster's email,
+# the article ID,
+# the parent comment (for replies) or an empty string (for comments),
+# and the comment's content
+# Adds the comment to the database and returns the newly formed comment as JSON
 @app.route("/api/postcomment", methods=["POST"])
 def postComment():
     try:
@@ -239,14 +331,19 @@ def postComment():
         debug_out("Posting Comment")
         debug_out(data)
 
+        # use email as username fallback
         username = data["email"]
+        # search all users documents for a matching email
         for u in mongo.searchDocument(DB_COMMENTS, COL_USERS, {"email": data["email"]}):
+            # retrieve their username
             username = u["username"]
 
         parentID = data["parentID"]
+        # if parentID is an empty string, change it to null
         if not parentID:
             parentID = None
 
+        # Create comment object
         commentData = Comment(
             username=username,
             useremail=data["email"],
@@ -255,12 +352,14 @@ def postComment():
             parentID=parentID,
         ).toJson()
 
+        # push to database
         mongo.insertDocument(
             DB_COMMENTS,
             COL_COMMENTS,
             commentData,
         )
 
+        # convert ObjectID to string and return the new comment's parsed data
         commentData["_id"] = str(commentData["_id"])
         debug_out(commentData)
         return jsonify({"status": "ok", "commentData": commentData})
@@ -269,10 +368,10 @@ def postComment():
         return jsonify({"Internal error": str(e)})
 
 
-COMMENT_REMOVAL_MESSAGE = "COMMENT REMOVED BY MODERATOR!"
-COMMENT_REDACTION_CHAR = "█"
-
-
+# Route to remove a comment via POST method
+# Requires a comment ID
+# Replaces the content of the specified comment in the database with a removal message
+# and returns the modified comment as JSON
 @app.route("/api/removecomment", methods=["POST"])
 def removeComment():
     try:
@@ -280,62 +379,98 @@ def removeComment():
         debug_out("Removing Comment")
         debug_out(data)
 
+        # replace comment content with removal message
+        # flag comment as removed
         replaceData = {"content": COMMENT_REMOVAL_MESSAGE, "removed": True}
+        # push changes to database
         mongo.updateDocument(
             DB_COMMENTS,
             COL_COMMENTS,
             replaceData.copy(),
-            {"_id": ObjectId(data["commentID"])},
+            {"_id": ObjectId(data["commentID"])},  # convert string to ObjectID
         )
+
+        # return the modified comment data
         return jsonify({"status": "ok", "commentData": replaceData})
     except Exception as e:
         raise e
         return jsonify({"Internal error": str(e)})
 
 
+# Route to redact part of a comment via POST method
+# Requires a comment ID, a start index, and end index
+# Replaces the specified slice of the specified comment in the database with redacted characters
+# and returns the modified comment as JSON
 @app.route("/api/redactcomment", methods=["POST"])
 def redactComment():
     try:
         data = request.form
         debug_out("Redacting Comment")
         debug_out(data)
+
+        # convert string to ObjectID and use it to search the database
         oid = ObjectId(data["commentID"])
         content = mongo.findDocument(DB_COMMENTS, COL_COMMENTS, {"_id": oid})["content"]
 
         startIndex = int(data["startIndex"])
         endIndex = int(data["endIndex"])
 
+        # replace a slice of the comment with redacted characters
         cHead = content[:startIndex]
         cRedact = COMMENT_REDACTION_CHAR * (endIndex - startIndex)
         cTail = content[endIndex:]
 
+        # replace comment content with spliced content
         replaceData = {"content": cHead + cRedact + cTail}
+        # push changes to database
         mongo.updateDocument(
             DB_COMMENTS,
             COL_COMMENTS,
             replaceData.copy(),
             {"_id": oid},
         )
+
+        # return the modified comment data
         return jsonify({"status": "ok", "commentData": replaceData})
     except Exception as e:
         raise e
         return jsonify({"Internal error": str(e)})
 
 
+# Route to check permissions of a user
+# Returns a JSON object
+# All existing permissions are included as keys,
+# with the given user's permissions having a value of true
 @app.route("/api/checkpermissions/<string:email>")
 def checkPermissions(email: str):
     perms = {}
+    # search all documents in the permissions collection
     for perm_doc in mongo.searchDocument(DB_COMMENTS, COL_PERMISSIONS):
+        # convert to dict
         d = dict(perm_doc)
         for k, v in d.items():
+            # each key in this doc is either the name of a permission or "_id"
             if k != "_id":
+                # if this permission hasn't been seen yet, add it to the return object
+                # this way, all permissions that exist will be part of the return object
                 if k not in perms:
                     perms[k] = False
+                # if this user has this permission, flag it as true
                 if email in v:
                     perms[k] = True
     return jsonify(perms)
 
 
+# endregion Comments Database CRUD operations
+
+
+# region Authentication
+
+
+# I have asked the TAs about login security, and they have explicitly stated that security is not an objective of this assignment.
+# In their example, a login is handled by redirecting with the account's email as a URL parameter.
+# A similar method is used here.
+# Provided by starter code, except return statements are modified.
 @app.route("/")
 def home():
     user = session.get("user")
@@ -346,6 +481,7 @@ def home():
     return redirect(f"http://localhost:5173")
 
 
+# Provided by starter code
 @app.route("/login")
 def login():
     session["nonce"] = nonce
@@ -353,6 +489,7 @@ def login():
     return oauth.flask_app.authorize_redirect(redirect_uri, nonce=nonce)
 
 
+# Provided by starter code
 @app.route("/authorize")
 def authorize():
     token = oauth.flask_app.authorize_access_token()
@@ -365,11 +502,14 @@ def authorize():
     return redirect("/")
 
 
+# Provided by starter code
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
 
+
+# endregion Authentication
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8000)
